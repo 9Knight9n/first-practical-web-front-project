48ee535b4be9222ccf24a0750e64e0940c145d90
Sajad <naghizadehmdsd@gmail.com>
2021-12-18 11:21:40 +0330

    applied some changes

--- server/web/login.php
+++ server/web/login.php
@@ -5,7 +5,19 @@
 
     require '../helper/dbconnect.php';
     print_r($_POST);
+    bin2hex()
 
-
+//    var_dump(false);
+//    print_r(false);
+//    $hello = array("hello"=>'hello world');
+//    $hello2 = explode(' ',"hello World");
+//    foreach($hello as $k => $value)
+//    {
+//        echo "key=",$k," val=",$value,"\n";
+//    }
+//    for ($i = 0 ; $i<count($hello);$i++)
+//    {
+//        continue;
+//    }
 
 ?>
